cb659b34fcabc13d4f19c765b6895f6c512297f8
MargheritaPanzani <98821879+margherita95444@users.noreply.github.com>
2022-05-10 13:24:49 +0200

    Add files via upload

---
+++ Products.js
@@ -0,0 +1,106 @@
+export default [
+  {
+    "id": 1,
+    "img": "../assets/images/bike.jpg",
+    "name": "Bike",
+    "description": "Text text text text text text text text text text text text text",
+    "productCategory": {
+        "id": 1,
+        "name": "Sport"
+      },
+    "price": 189,
+    "quantity": 0,
+    "soldout": 15
+  },
+  {
+    "id": 2,
+    "img":"../assets/images/charger.jpg",
+    "name": "Charger",
+    "description": "Text text text text text text text text text text text text text",
+    "productCategory": {
+      "id": 2,
+      "name": "Electronic"
+    },
+    "price": 3,
+    "quantity": 0,
+    "soldout": 8
+  },
+  {
+    "id": 3,
+    "img": "../assets/images/mouse.jpg",
+    "name": "Mouse",
+    "description": "Text text text text text text text text text text text text text",
+    "productCategory": {
+      "id": 3,
+      "name": "Tech"
+    },
+    "price": 5,
+    "quantity": 0,
+    "soldout": 4
+  },
+  {
+    "id": 4,
+    "img": "../assets/images/plant.jpg",
+    "name": "Plant",
+    "description": "Text text text text text text text text text text text text text",
+    "productCategory": {
+      "id": 4,
+      "name": "Gardening"
+    },
+    "price": 45,
+    "quantity": 0,
+    "soldout": 7
+  },
+  {
+    "id": 5,
+    "img": "../assets/images/scissors.jpg",
+    "name": "Scissors",
+    "description": "Text text text text text text text text text",
+    "productCategory": {
+      "id": 5,
+      "name": "Cooking"
+    },
+    "price": 7,
+    "quantity": 0,
+    "soldout": 22
+  },
+  {
+    "id": 6,
+    "img": "../assets/images/smartphone.jpg",
+    "name": "Smarphone",
+    "description": "Text text text text text text text text text",
+    "productCategory": {
+      "id": 2,
+      "name": "Electronic"
+    },
+    "price": 200,
+    "quantity": 0,
+    "soldout": 9
+  },
+  {
+    "id": 7,
+    "img": "../assets/images/T-shirt.jpg",
+    "name": "T-shirt",
+    "description": "Text text text text text text text text text",
+    "productCategory": {
+      "id": 6,
+      "name": "Clothing"
+    },
+    "price": 24.99,
+    "quantity": 0,
+    "soldout": 3
+  },
+  {
+    "id": 8,
+    "img": "../assets/images/watch.jpg",
+    "name": "Watch",
+    "description": "Text text text text text text text text text",
+    "productCategory": {
+      "id": 7,
+      "name": "Objects"
+    },
+    "price": 215,
+    "quantity": 0,
+    "soldout": 5
+  }
+]
